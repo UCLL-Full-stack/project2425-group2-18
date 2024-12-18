3337c184f4ab05ca5acb402ecd4dc402d12bdac7
MaximeBrepoels <maxime.brepoels@student.ucll.be>
2024-12-18 23:18:11 +0100

    MovieOverview with dummy data

--- front-end/components/movies/MovieOverview.tsx
+++ front-end/components/movies/MovieOverview.tsx
@@ -2,6 +2,13 @@
 
 import { useRouter } from "next/router";
 
+interface Movie {
+    id: number;
+    name: string;
+    director: string;
+    releaseYear: number;
+}
+
 interface MovieProps {
     movies: Movie[];
 }
@@ -12,13 +19,17 @@ const MovieOverview: React.FC<MovieProps> = ({ movies }) => {
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
-            {movies.map((movie) => (
-                <div key={movie.id} className="bg-white shadow-md rounded-lg p-4 m-4">
-                    <h2 className="text-xl font-bold mb-2">{movie.name}</h2>
-                    <p className="text-gray-700 mb-1">Director: {movie.director}</p>
-                    <p className="text-gray-700">Release Year: {movie.releaseYear}</p>
-                </div>
-            ))}
+            {movies.length === 0 ? (
+                <p className="text-center text-l mb-2 col-span-full pt-5">No movies yet!</p>
+            ) : (
+                movies.map((movie) => (
+                    <div key={movie.id} className="bg-white shadow-md rounded-lg p-4 m-4">
+                        <h2 className="text-xl font-bold mb-2">{movie.name}</h2>
+                        <p className="text-gray-700 mb-1">Director: {movie.director}</p>
+                        <p className="text-gray-700">Release Year: {movie.releaseYear}</p>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
--- front-end/pages/movie/index.tsx
+++ front-end/pages/movie/index.tsx
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Header from "@/components/header";
 import Link from "next/link";
+import MovieOverview from "@/components/movies/MovieOverview";
+import { Movie } from "@/components/movies/MovieOverview";
 
 const Movie: React.FC = () => {
+    const movies: Movie[] = [
+        { id: 1, name: "Inception", director: "Christopher Nolan", releaseYear: 2010 },
+        { id: 2, name: "The Matrix", director: "Lana Wachowski, Lilly Wachowski", releaseYear: 1999 },
+        { id: 3, name: "Interstellar", director: "Christopher Nolan", releaseYear: 2014 },
+        { id: 4, name: "The Dark Knight", director: "Christopher Nolan", releaseYear: 2008 },
+        { id: 5, name: "Pulp Fiction", director: "Quentin Tarantino", releaseYear: 1994 },
+    ];
+
     return (
         <>
             <Head>
@@ -11,6 +21,9 @@ const Movie: React.FC = () => {
             <Header />
             <main className="bg-custom-blue min-h-screen flex flex-col items-center">
                 <h1 className="text-white font-semibold text-center text-2xl">Movie Overview</h1>
+                <section>
+                    <MovieOverview movies={movies}/>
+                </section>
                 <Link href="/addmovie">
                     <button className="bg-green-button-home text-white font-semibold bg-hover-button-home mt-8 w-12 h-12 rounded-full flex items-center justify-center">
                         {
